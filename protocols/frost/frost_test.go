08175e427fb5bc5ea5cbabfbd54d36b7ab5e635e
lynndell8530 <lynndell.zhong@osl.com>
2025-11-26 22:06:29 +0800

    minus ll

--- protocols/frost/frost_test.go
+++ protocols/frost/frost_test.go
@@ -77,7 +77,7 @@ func do(t *testing.T, id party.ID, ids []party.ID, threshold int, message []byte
 	assert.True(t, cTaproot.PublicKey.Verify(taprootSignature, message))
 }
 
-// lynndell
+// lynnd
 func TestFrost(t *testing.T) {
 	N := 5
 	T := N - 1
